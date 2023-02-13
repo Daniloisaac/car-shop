6808c1ece7963a7ce905b4ff3df19485437e8f82
danilo isaac <daniloisaac942@gmail.com>
2023-02-13 19:51:35 -0300

    feat: adicionei rota cars e um middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import car from './Routes/Car.Routes';
 
 const app = express();
-
+app.use(express.json());
+app.use('/cars', car);
+app.use(ErrorHandler.handle);
 export default app;
