4cd3f6cae0fba1a603868981ec70e831ba5f45d9
danilo isaac <daniloisaac942@gmail.com>
2023-02-14 09:41:08 -0300

    feat: adiconei metodo getCars e getCarById no service

--- src/Services/Car.service.ts
+++ src/Services/Car.service.ts
@@ -17,4 +17,39 @@ export default class CarService {
     const newCar = await carODM.create(car);
     return this.createCarDomain(newCar);
   }
+
+  public async getCars() {
+    const carODM = new Car();
+    const allCars = await carODM.getCars();
+    const cars = allCars.map((car) => ({
+      id: car._id,
+      model: car.model, 
+      year: car.year,
+      color: car.color,
+      status: car.status,
+      buyValue: car.buyValue, 
+      doorsQty: car.doorsQty,
+      seatsQty: car.seatsQty,
+    }));
+    return cars;
+  }
+  public async getCarById(id: string) {
+    const carODM = new Car();
+
+    const carById = await carODM.getCarById(id);
+    if (!carById) return { status: 404, messageError: 'Car not found' }; 
+    if (carById) {
+      const car = [{
+        id: carById._id,
+        model: carById.model, 
+        year: carById.year,
+        color: carById.color,
+        status: carById.status,
+        buyValue: carById.buyValue, 
+        doorsQty: carById.doorsQty,
+        seatsQty: carById.seatsQty,
+      }];
+      return { status: 200, message: car };
+    }
+  }
 }
