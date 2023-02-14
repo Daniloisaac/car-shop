479d409de78236e9a423adbb168bacaa6e099097
danilo isaac <daniloisaac942@gmail.com>
2023-02-14 09:42:22 -0300

    feat: adiconei rota get / e rota get/:id que chama o controller

--- src/Routes/Car.Routes.ts
+++ src/Routes/Car.Routes.ts
@@ -4,5 +4,7 @@ import CarController from '../Controllers/Car.controller';
 const routes = Router();
 
 routes.post('/', (req, res, next) => new CarController(req, res, next).create());
+routes.get('/', (req, res, next) => new CarController(req, res, next).getCars());
+routes.get('/:id', (req, res, next) => new CarController(req, res, next).getCarById());
 
 export default routes;
