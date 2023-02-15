8b0aa1ae38e6e9f3d9bd40309033cda3fbf818be
danilo isaac <daniloisaac942@gmail.com>
2023-02-15 10:29:31 -0300

    feat: creiei IMotorcycle interface que lida com os atributos de motos

---
+++ src/Interfaces/IMotorcycle.ts
@@ -0,0 +1,10 @@
+export default interface IMotorCycles {
+  id?: string,
+  model: string,
+  year: number,
+  color: string,
+  status?: boolean,
+  buyValue: number,
+  category: string,
+  engineCapacity: number,
+}
