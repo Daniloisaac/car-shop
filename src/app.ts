828aca2dc6fe6f7cf1b07313ec33bcb8a09ddb02
danilo isaac <daniloisaac942@gmail.com>
2023-02-15 10:40:52 -0300

    feat: adicionei rota motorcyle

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandler';
 import car from './Routes/Car.Routes';
+import motorcycles from './Routes/MotorCycle.Routes';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', car);
+app.use('/motorcycles', motorcycles);
 app.use(ErrorHandler.handle);
 export default app;
